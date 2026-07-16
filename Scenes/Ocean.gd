e5bb527ec7b2cc8f15200660de8df6e7dc35ccdd
ryanstatton <91967147+ryanstatton@users.noreply.github.com>
2026-07-16 17:05:10 +0100

    Fly Plane
    Did some basic moving stuff. Rotation and moving along y and x axis globaaly and locally

--- Scenes/Ocean.gd
+++ Scenes/Ocean.gd
@@ -5,12 +5,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("ready")
+	print("it works")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.position.x += 100.0 * delta
+	plane.move_local_x(100.0 * delta)
+	helicopter.move_local_x(50 * delta)
+	helicopter.rotate(2 * delta)
+	
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(1.5 * delta)
+		
+	if Input.is_action_pressed("ui_right"):
+		plane.rotate(-1.5 * delta)
 	
 	pass
