097a0cc0dec71109eb77f025f94a699a63904131
ryanstatton <ryanstatton.rs@gmail.com>
2026-08-17 14:57:53 +0100

    Control Flow
    Logic on things involving the fellowship

---
+++ Scripting/ControlFlow.gd
@@ -0,0 +1,35 @@
+@tool
+extends EditorScript
+
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run() -> void:
+	var ring_bearer: String = "Boromir"
+	if ring_bearer == "Frodo":
+		print("Take it to the fire from once it came!")
+	elif ring_bearer == "Boromir":
+		print("You cannot wield it!")
+	else:
+		print("Gollum has the ring.")
+	
+	var arrows: int = 10
+	while arrows > 0:
+		arrows -= 1
+		if arrows == 7:
+			continue
+		if arrows == 4:
+			break
+		print(arrows, " arrows left.")
+		
+	
+	var fellowship: Array[String] = [
+		"Frodo", "Sam", "Aragorn", "Gimli",
+		"Legolas", "Gandalf", "Boromir", "Merry",
+		"Pippin"
+	]
+	
+	for member in fellowship:
+		if member == "Gimli":
+			continue
+		print(member)
+	
